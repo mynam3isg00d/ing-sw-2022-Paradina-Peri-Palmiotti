5441459a35e592db3da9e6f24e35a077b4305938
Samuele Peri <95347137+john-galt-10@users.noreply.github.com>
2022-06-16 22:57:55 +0200

    Centaur effect tested

--- src/test/java/Controller/CharacterControllerTest.java
+++ src/test/java/Controller/CharacterControllerTest.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Controller.CharacterEffects.CentaurEffect;
 import Controller.CharacterEffects.Strategies.CentaurStrategy;
+import Controller.CharacterEffects.Strategies.DefaultInfluenceStrategy;
 import Controller.CharacterEffects.Strategies.DefaultProfessorStrategy;
 import Controller.CharacterEffects.Strategies.FarmerStrategy;
 import Events.EventFactory;
@@ -63,10 +64,26 @@ class CharacterControllerTest {
         g.setCharacterController(new CharacterController(g, new Integer[]{5, 5, 5}));
         CharacterController cc = g.getCharacterController();
 
+        //puts one blue student on island 0
+        g.islandController.moveStudent(0, Student.BLUE);
+
+        //assigns blue professor to p1
+        g.boardsController.setProfessor("B", pl.get(1));
+        //sets the influence on island 0 to p0's team
+        g.islandController.getIslandModel().setInfluence(0, pl.get(0).getTeamID());
+
+        //calculates influence on island 0 - since the tower does count, should return -1 it's a tie
+        assertTrue(g.getIslandController().getInfluenceStrategy() instanceof DefaultInfluenceStrategy);
+        assertEquals(-1, g.islandController.getInfluenceStrategy().calcInfluence(0, g.islandController.getIslandModel(), g.boardsController));
+
+        //p0 plays the centaur
         assertThrows(InsufficientCoinsException.class, () -> { cc.buyCard(0, pl.get(0).getPlayerID(), new ArrayList<String>());});
         cc.giveCoins(pl.get(0).getPlayerID(), 9999);
         cc.buyCard(0, pl.get(0).getPlayerID(), new ArrayList<String>());
         assertTrue(g.getIslandController().getInfluenceStrategy() instanceof CentaurStrategy);
+
+        //calculates influence on island 0 - since the tower doesn't count, should return p1's team id
+        assertEquals(pl.get(1).getTeamID(), g.islandController.getInfluenceStrategy().calcInfluence(0, g.islandController.getIslandModel(), g.boardsController));
     }
 
     @Test
@@ -76,12 +93,32 @@ class CharacterControllerTest {
         g.setCharacterController(new CharacterController(g, new Integer[]{9, 9, 9}));
         CharacterController cc = g.getCharacterController();
 
+        //puts a green and a blue student in p1's dining
         g.boardsController.addToDining(pl.get(0).getPlayerID(), Student.BLUE);
         g.boardsController.addToDining(pl.get(0).getPlayerID(), Student.GREEN);
 
-        List<String> playerInput = List.of("1", "2", "1", "2");
+        //makes room for two red students in p1's entrance
+        g.boardsController.removeFromEntrance(pl.get(0).getPlayerID(), 0);
+        g.boardsController.removeFromEntrance(pl.get(0).getPlayerID(), 1);
+        List<Student> toAdd = new ArrayList<>();
+        toAdd.add(Student.RED);
+        toAdd.add(Student.RED);
+        g.boardsController.addToEntrance(pl.get(0).getPlayerID(), toAdd);
+
+        //checks the green and blue student actually are in the dinings
+        assertEquals(1, g.boardsController.getBoard(pl.get(0).getPlayerID()).getDinings()[Student.BLUE.getColorId()]) ;
+        assertEquals(1, g.boardsController.getBoard(pl.get(0).getPlayerID()).getDinings()[Student.GREEN.getColorId()]) ;
+        assertEquals(0, g.boardsController.getBoard(pl.get(0).getPlayerID()).getDinings()[Student.RED.getColorId()]) ;
+
+        List<String> playerInput = List.of("0", "1", "1", "2");
         cc.giveCoins(pl.get(0).getPlayerID(), 9999);
         cc.buyCard(0, pl.get(0).getPlayerID(), playerInput);
+
+        //red dining should now have two students
+        assertEquals(0, g.boardsController.getBoard(pl.get(0).getPlayerID()).getDinings()[Student.GREEN.getColorId()]) ;
+        assertEquals(0, g.boardsController.getBoard(pl.get(0).getPlayerID()).getDinings()[Student.BLUE.getColorId()]) ;
+        assertEquals(2, g.boardsController.getBoard(pl.get(0).getPlayerID()).getDinings()[Student.RED.getColorId()]) ; /* FALSE */
+
     }
 
     @Test
