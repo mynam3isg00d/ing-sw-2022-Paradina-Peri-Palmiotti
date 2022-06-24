b431fc73ad38064038003b353193e8adef0de788
marcoparadina <mparadina10@gmail.com>
2022-06-24 20:24:19 +0200

    Added fxml and controller for farmer character card

---
+++ src/main/java/View/GUI/Controllers/FarmerController.java
@@ -0,0 +1,19 @@
+package View.GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class FarmerController extends CharacterController{
+    @FXML
+    Pane main;
+
+    public void sendOk(ActionEvent actionEvent) {
+        super.sendOk(actionEvent, main);
+        Stage stage = (Stage)((Node)actionEvent.getTarget()).getScene().getWindow();
+        stage.close();
+    }
+}
