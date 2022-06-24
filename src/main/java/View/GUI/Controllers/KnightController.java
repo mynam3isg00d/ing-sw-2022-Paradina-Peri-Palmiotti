e84f742251e6ab58f9967cc5cdaaf916f604342f
marcoparadina <mparadina10@gmail.com>
2022-06-24 20:57:10 +0200

    Added fxml and controller for knight character card. Other minor fixes

---
+++ src/main/java/View/GUI/Controllers/KnightController.java
@@ -0,0 +1,18 @@
+package View.GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class KnightController extends CharacterController {
+    @FXML
+    Pane main;
+
+    public void sendOk(ActionEvent actionEvent) {
+        super.sendOk(actionEvent, main);
+        Stage stage = (Stage)((Node)actionEvent.getTarget()).getScene().getWindow();
+        stage.close();
+    }
+}
